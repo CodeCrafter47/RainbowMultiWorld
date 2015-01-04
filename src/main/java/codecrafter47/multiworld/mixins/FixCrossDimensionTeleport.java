584c0d0f75fc49ba04501c5485115743abe24416
Florian Stober <Flo.e3@gmx.de>
2015-01-04 17:22:24 +0100

    teleport again

--- src/main/java/codecrafter47/multiworld/mixins/FixCrossDimensionTeleport.java
+++ src/main/java/codecrafter47/multiworld/mixins/FixCrossDimensionTeleport.java
@@ -15,107 +15,82 @@ import joebkt.*;
 import lombok.SneakyThrows;
 import net.minecraft.server.MinecraftServer;
 
-import java.lang.reflect.Field;
-
-/**
- * Created by florian on 27.11.14.
- */
 @Mixin(WrapperObjects.Entities.PlayerWrapper.class)
 public class FixCrossDimensionTeleport extends EntityWrapper {
 
-	@Shadow
-	public EntityPlayer plr;
+    @Shadow
+    public EntityPlayer plr;
 
-	public FixCrossDimensionTeleport(EntityGeneric argEnt) {
-		super(argEnt);
-	}
+    public FixCrossDimensionTeleport(EntityGeneric argEnt) {
+        super(argEnt);
+    }
 
-	@Overwrite
-	@SneakyThrows
-	public void teleport(MC_Location loc) {
-		if (loc != null) {
-			plr.setVehicle(null);
-			_SerializableLocation sloc = new _SerializableLocation(loc.x, loc.y, loc.z, loc.dimension, loc.yaw, loc.pitch);
-			if (loc.dimension != this.plr.dimension) {
-				// this.plr.handleChangeDimension(loc.dimension, false); no no no
-				int newDimen = loc.dimension;
-				MC_EventInfo ei = new MC_EventInfo();
-				PlayerWrapper pWrap = new PlayerWrapper(this.plr);
+    @Overwrite
+    @SneakyThrows
+    public void teleport(MC_Location loc) {
+        if (loc != null) {
+            plr.setVehicle(null);
+            _SerializableLocation sloc = new _SerializableLocation(loc.x, loc.y, loc.z, loc.dimension, loc.yaw, loc.pitch);
+            if (loc.dimension != this.plr.dimension) {
+                int newDimen = loc.dimension;
+                MC_EventInfo ei = new MC_EventInfo();
+                PlayerWrapper pWrap = new PlayerWrapper(this.plr);
 
-				for (Object var2 : _JoeUtils.plugins) {
-					try {
-						((PluginInfo) var2).ref.onAttemptPlayerChangeDimension(pWrap, newDimen, ei);
-					}
-					catch (Throwable var8) {
-						var8.printStackTrace();
-					}
-				}
+                for (Object var2 : _JoeUtils.plugins) {
+                    try {
+                        ((PluginInfo) var2).ref.onAttemptPlayerChangeDimension(pWrap, newDimen, ei);
+                    } catch (Throwable var8) {
+                        var8.printStackTrace();
+                    }
+                }
 
-				if (_JoeUtils.DebugMode) {
-					String var21 = String.format("JKC DEBUG: --- DimensionChange (%s) to --- (%s) --- %s @ %s", new Object[] { _JoeUtils.GetDimensionName(plr.dimension), _JoeUtils.GetDimensionName(newDimen), plr.getName(), _JoeUtils.GetEntityLocationDescription(plr) });
-					System.out.println(var21);
-				}
+                if (_JoeUtils.DebugMode) {
+                    String var21 = String.format("JKC DEBUG: --- DimensionChange (%s) to --- (%s) --- %s @ %s", new Object[]{_JoeUtils.GetDimensionName(plr.dimension), _JoeUtils.GetDimensionName(newDimen), plr.getName(), _JoeUtils.GetEntityLocationDescription(plr)});
+                    System.out.println(var21);
+                }
 
-				PlayerList playerList = plr.b_server.getThePlayerList();
-				//playerList.notifyAboutChangedDimension(plr, newDimen);
-				{
-					int plrDimen = plr.dimension;
-					WorldServer localWorldFrom = MinecraftServer.getServer().getWorldServerByDimension(plr.dimension);
-					plr.dimension = newDimen;
-					WorldServer localWorldTo = MinecraftServer.getServer().getWorldServerByDimension(plr.dimension);
-					int packetDimen = plr.dimension;
-					if (packetDimen > 1) {
-						Environment i = PluginMultiWorld.getInstance().getStorageManager().getCustomConfig(packetDimen).getEnvironment();
-						GenerationType generationType = PluginMultiWorld.getInstance().getStorageManager().getCustomConfig(packetDimen).getGenerationType();
-						if (generationType == GenerationType.END) {
-							packetDimen = 1;
-						}
-						else if (generationType == GenerationType.NETHER || i == codecrafter47.multiworld.api.Environment.NETHER) {
-							packetDimen = -1;
-						}
-						else if (i == codecrafter47.multiworld.api.Environment.END) {
-							packetDimen = 1;
-						}
-						else {
-							packetDimen = 0;
-						}
-					}
-
-					plr.plrConnection.sendPacket(new Packet_Respawn(packetDimen, plr.worldEnt.getDifficulty(), plr.worldEnt.getWorldData().getLevelType(), plr.playerInteractManager.getGameMode()));
-					localWorldFrom.removeEntity(plr);
-					plr.dead = false;
-					//playerList.changeDimensionDetailed(plr, plrDimen, localWorldFrom, localWorldTo);
-					{
-						plr.setPositionRotation(loc.x + 0.5, loc.y + 0.5, loc.z + 0.5, loc.yaw, loc.pitch);
-						if (plr.isNotDead()) {
-							localWorldFrom.entRelatedChunkCheck(plr, false);
-						}
-						localWorldTo.addEntity(plr);
-						localWorldTo.entRelatedChunkCheck(plr, false);
-
-						plr.setEntityWorld(localWorldTo);
-					}
-					playerList.handlePlayerReceivingNewChunks(plr, localWorldFrom);
-					plr.plrConnection.teleportWithYawPitch(plr.xCoord, plr.yCoord, plr.zCoord, plr.yaw, plr.pitch);
-					plr.playerInteractManager.setInnerWorld(localWorldTo);
-					playerList.sendPlayerPacketsAboutNewDimension(plr, localWorldTo);
-					playerList.sendPlayerPacketAboutInventoryChanged(plr);
-
-					for (Object potionEffect : plr.getActivePotionEffects()) {
-						plr.plrConnection.sendPacket(new Packet_EntityEffect(plr.getEntityId(), (PotionEffect) potionEffect));
-					}
-				}
-				plr.lastSentExp = -1;
-				Field bK = Class.forName("joebkt.EntityPlayer").getDeclaredField("bK");
-				bK.setAccessible(true);
-				bK.set(plr, -1.0F);
-				Field bL = Class.forName("joebkt.EntityPlayer").getDeclaredField("bL");
-				bL.setAccessible(true);
-				bL.set(plr, -1);
-			} else {
-				this.plr.TeleportSLoc(sloc);
-			}
-		}
-	}
+                WorldServer localWorldFrom = MinecraftServer.getServer().getWorldServerByDimension(plr.dimension);
+                localWorldFrom.getEntityTracker().removePlayer(plr);
+                localWorldFrom.getPlayerChunkMap().removePlayer(plr);
+                MinecraftServer.getServer().getThePlayerList().players.remove(plr);
+                plr.worldEnt.removeEntity(plr);
+                plr.dimension = newDimen;
+                WorldServer localWorldTo = MinecraftServer.getServer().getWorldServerByDimension(plr.dimension);
+                plr.setEntityWorld(localWorldTo);
+                plr.dead = false;
+                plr.setPositionRotation(loc.x, loc.y, loc.z, loc.yaw, loc.pitch);
+                localWorldTo.cachedChunks.getOrCreateChunk((int) plr.xCoord >> 4, (int) plr.zCoord >> 4);
+                while (!localWorldTo.getNearbyEntities(plr, plr.funcAppliesIfCanBePushed()).isEmpty() && plr.yCoord < 256.0D) {
+                    plr.setPositionRotation(plr.xCoord, plr.yCoord + 1.0D, plr.zCoord, plr.yaw, plr.pitch);
+                }
+                plr.dead = false;
+                int packetDimen = plr.dimension;
+                if (packetDimen > 1) {
+                    Environment i = PluginMultiWorld.getInstance().getStorageManager().getCustomConfig(packetDimen).getEnvironment();
+                    GenerationType generationType = PluginMultiWorld.getInstance().getStorageManager().getCustomConfig(packetDimen).getGenerationType();
+                    if (generationType == GenerationType.END) {
+                        packetDimen = 1;
+                    } else if (generationType == GenerationType.NETHER || i == codecrafter47.multiworld.api.Environment.NETHER) {
+                        packetDimen = -1;
+                    } else if (i == codecrafter47.multiworld.api.Environment.END) {
+                        packetDimen = 1;
+                    } else {
+                        packetDimen = 0;
+                    }
+                }
+                plr.plrConnection.sendPacket(new Packet_Respawn(packetDimen, plr.worldEnt.getDifficulty(), plr.worldEnt.getWorldData().getLevelType(), plr.playerInteractManager.getGameMode()));
+                plr.plrConnection.sendPacket(new Packet_SetExperience(plr.m_exp, plr.xpTotal, plr.xpLevel));
+                plr.setSneaking(false);
+                MinecraftServer.getServer().getThePlayerList().sendPlayerPacketsAboutNewDimension(plr, localWorldTo);
+                localWorldTo.getPlayerChunkMap().addPlayer(plr);
+                localWorldTo.addEntity(plr);
+                MinecraftServer.getServer().getThePlayerList().players.add(plr);
+                plr.playerInteractManager.setInnerWorld(localWorldTo);
+                plr.setHealth(plr.getHealth());
+            } else {
+                this.plr.TeleportSLoc(sloc);
+            }
+        }
+    }
 
 }
